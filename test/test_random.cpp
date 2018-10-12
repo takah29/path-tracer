a608e5c941562489bf87a453b533324c740e1cef
takah29 <takah0209@gmail.com>
2018-10-13 03:23:34 +0900

    一様乱数のテストを追加

---
+++ test/test_random.cpp
@@ -0,0 +1,27 @@
+#define BOOST_TEST_MODULE random
+#include <boost/test/included/unit_test.hpp>
+
+#include "../random.h"
+
+BOOST_AUTO_TEST_SUITE(test_uniform_real_generator_sute)
+BOOST_AUTO_TEST_CASE(test_correct_rand) {
+    UniformRealGenerator rnd;
+
+    int N = 1000;
+    double result;
+
+    for (int i = 0; i < N; i++) {
+        result = rnd();
+        BOOST_CHECK_GE(result, 0.0);
+        BOOST_CHECK_LE(result, 1.0);
+    }
+
+    rnd = UniformRealGenerator(0, 100.0, 200.0);
+    for (int i = 0; i < N; i++) {
+        result = rnd();
+        BOOST_CHECK_GE(result, 100.0);
+        BOOST_CHECK_LE(result, 200.0);
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
