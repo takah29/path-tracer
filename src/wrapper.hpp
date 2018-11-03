a22333d861cda22c48d59c3f1d0333171fd4b27e
takah29 <takah0209@gmail.com>
2018-11-04 05:27:25 +0900

    stb_imageライブラリ用の画像ファイル読み込みラッパーを作成

---
+++ src/wrapper.hpp
@@ -0,0 +1,28 @@
+#ifndef _WRAPPER_H_
+#define _WRAPPER_H_
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <string>
+#include "../stb/stb_image.h"
+#include "utility.hpp"
+#include "vec.hpp"
+
+Image load_rgb_image_file(const std::string &file_path) {
+    int x, y, n;
+    unsigned char *data = stbi_load(file_path.c_str(), &x, &y, &n, 0);
+
+    Image image(x, y);
+    int ch = 3;
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            double r = *(data + (ch * (i * x + j))) / 256.0;
+            double g = *(data + (ch * (i * x + j) + 1)) / 256.0;
+            double b = *(data + (ch * (i * x + j) + 2)) / 256.0;
+            image.set_color(Color(r, g, b), i, j);
+        }
+    }
+
+    return image;
+}
+
+#endif
