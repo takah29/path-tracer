a02e2941739ad150ce5479405468598296c224be
takah29 <takah0209@gmail.com>
2018-11-04 23:09:01 +0900

    引数を変更

--- src/wrapper.hpp
+++ src/wrapper.hpp
@@ -7,11 +7,11 @@
 #include "utility.hpp"
 #include "vec.hpp"
 
-Image load_rgb_image_file(const std::string &file_path) {
+void load_rgb_image_file(const std::string &file_path, Image &image) {
     int x, y, n;
     unsigned char *data = stbi_load(file_path.c_str(), &x, &y, &n, 0);
 
-    Image image(x, y);
+    image.set_image_size(x, y);
     int ch = 3;
     for (int i = 0; i < y; i++) {
         for (int j = 0; j < x; j++) {
@@ -21,8 +21,6 @@ Image load_rgb_image_file(const std::string &file_path) {
             image.set_color(Color(r, g, b), i, j);
         }
     }
-
-    return image;
 }
 
 #endif
