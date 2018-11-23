b9388007762fad15e73b32d766c93c6fee86c755
takah29 <takah0209@gmail.com>
2018-11-24 00:32:45 +0900

    run.shを修正

--- run.sh
+++ run.sh
@@ -1,10 +1,18 @@
+#!/bin/bash
 make
 
-mkdir models
+if [ ! -d "models" ]; then
+  mkdir models
+fi
+
 cd models
-wget http://graphics.stanford.edu/pub/3Dscanrep/bunny.tar.gz
-tar -zxvf bunny.tar.gz
-rm bunny.tar.gz
+
+if [ ! -d "bunny" ]; then
+  wget http://graphics.stanford.edu/pub/3Dscanrep/bunny.tar.gz
+  tar -zxvf bunny.tar.gz
+  rm bunny.tar.gz
+fi
+
 cd ..
 
 ./main -n 3 -w 320 -h 240 -s 2 -ss 8
